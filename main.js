edd2e59a58ab57bd8ded4561984d47c010a69f6a
Tosh <128783853+ItsTosh@users.noreply.github.com>
2024-07-10 23:08:30 +1000

    Added Operate function
    Added operate function - operates basic math functions

--- main.js
+++ main.js
@@ -21,3 +21,20 @@ function divide(num1, num2) {
   return num1 / num2
 }
 
+function operate(num1, num2, operator) {
+  console.log(`
+    num1: ${num1}
+    num2: ${num2};
+    operator: ${operator};  
+  `)
+
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "x") {
+    return multiply(num1, num2)
+  } else if (operator === "/") {
+    return divide(num1, num2)
+  }
+}
