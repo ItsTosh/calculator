7bf9a7504390a2b4945d5ca59ce9577cccb9f60c
Tosh <128783853+ItsTosh@users.noreply.github.com>
2024-07-12 17:11:00 +1000

    Completed basic display
    Added math equation display + answer

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-let num1;
-let num2;
-let operator;
+const displayScreen = document.querySelector("#displayScreen");
+const answerScreen = document.querySelector("#answerScreen");
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operator")
+let num1 = "";
+let num2 = "";
+let operator = "";
 
 function add(num1, num2) {
   return num1 + num2;
@@ -32,9 +36,51 @@ function operate(num1, num2, operator) {
     return add(num1, num2);
   } else if (operator === "-") {
     return subtract(num1, num2);
-  } else if (operator === "x") {
+  } else if (operator === "X") {
     return multiply(num1, num2)
-  } else if (operator === "/") {
+  } else if (operator === "÷") {
     return divide(num1, num2)
   }
 }
+  
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    if (operator === "") {
+      num1 = Number(event.target.innerHTML);
+      displayScreen.textContent = num1;
+      console.log(num1);
+    } else {
+      num2 = Number(event.target.innerHTML);
+      displayScreen.textContent = `${num1} ${operator} ${num2} =  `
+      console.log(num2);
+    }
+
+    console.log(`num1: ${num1}, num2: ${num2}`)
+  })
+})
+
+operators.forEach((op) => {
+  op.addEventListener("click", (event) => {
+    const eventTarget = event.target.innerHTML;
+
+    if (eventTarget !== "=") {
+      operator = eventTarget;
+      displayScreen.textContent = `${num1} ${operator}`
+      console.log(operator)
+    } else {
+      let answer = operate(num1, num2, operator);
+      answerScreen.textContent = answer;
+      num1 = "";
+      num2 = "";
+      operator = "";
+    }
+
+    if (eventTarget === "=" && num2 === "") {
+      num2 = num1;
+      console.log(operate(num1, num2, operator))
+      num1 = "";
+      num2 = "";
+      operator = "";
+    }
+  })
+})
